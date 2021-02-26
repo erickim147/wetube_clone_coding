df522f7e0651a0acb511687e9678501d58310fb7
eric Kim <gudals147@naver.com>
2021-02-27 02:02:04 +0900

    21.02.26_class_2.22

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video a",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creater: {
+      id: 121212,
+      name: "eric",
+      email: "eric@sdfs.com",
+    },
+  },
+  {
+    id: 324123,
+    title: "Video b",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creater: {
+      id: 121212,
+      name: "eric",
+      email: "eric@sdfs.com",
+    },
+  },
+  {
+    id: 44444393,
+    title: "Video c",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creater: {
+      id: 121212,
+      name: "eric",
+      email: "eric@sdfs.com",
+    },
+  },
+  {
+    id: 7789454,
+    title: "Video d",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creater: {
+      id: 121212,
+      name: "eric",
+      email: "eric@sdfs.com",
+    },
+  },
+];
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,13 +1,15 @@
 import express from "express";
+import {
+  changePassword,
+  editProfile,
+  userDetail,
+} from "../controllers/userController";
 import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send("Users"));
-userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
-userRouter.get(routes.changePassword, (req, res) =>
-  res.send("Change Password")
-);
-userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
